1a2071b19b12d248031131774150a80ed43c7ed1
Tejas Pancholi <tejas.er@outlook.com>
2017-07-20 15:55:29 +0530

    added sass and babel loaders

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-document.write('hello, webpack!')
+require('./styles.scss');
+
+
+class Car {
+  manufacturer(car){
+    document.write(`I have a ${car}`)
+  }
+}
+
+const bmw = new Car;
+bmw.manufacturer('bmw');
