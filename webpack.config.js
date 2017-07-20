bc1b42633361c87876e022953ea4c6675aaa0a0b
Tejas Pancholi <tejas.er@outlook.com>
2017-07-20 16:34:27 +0530

    Updated app structure and added dev server with hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,39 @@
 const webpack = require('webpack');
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 let config = {
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
+    path: path.resolve(__dirname, './public'),
     filename: 'output.js'
   },
   module: {
     rules: [
       {
-      test: /\.js$/, // files ending with .js
-      exclude: /node_modules/, // exclude the node_modules directory
-      loader: "babel-loader" // use this (babel-core) loader
-    },
-    {
-      test: /\.scss$/, //files ending with scss
-      loader: ['style-loader', 'css-loader', 'sass-loader'] // use this loaders
-    }
-    ]
-  }
+        test: /\.js$/, // files ending with .js
+        exclude: /node_modules/, // exclude the node_modules directory
+        loader: "babel-loader" // use this (babel-core) loader
+      },
+      {
+        test: /\.scss$/, //files ending with scss
+        use: ExtractTextWebpackPlugin.extract({
+          use: ['css-loader', 'sass-loader'], // use this loaders
+          fallback: 'style-loader' // fallback for any css not extracted
+        }) // end extract
+      }
+    ] // end rules
+  },
+  plugins: [
+    new ExtractTextWebpackPlugin('styles.css') // call the ExtractTextWebpackPlugin and pass the file name
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'), // a directory or url to serve html content from
+    historyApiFallback: true, // fallback to index.html incase of single page app
+    inline: true, // inline mode(set to false to disable including client script (like live-reload))
+    open: true // open default browser
+  },
+  devtool: 'eval-source-map' //enable devtools for better debugging behavior
 }
 
 module.exports = config;
